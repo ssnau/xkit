853d6f8dfc221877d09a12b0cc82131eb09f52aa
ssnau <korige@gmail.com>
2017-06-03 12:22:34 +0800

    update

--- fs/replace.js
+++ fs/replace.js
@@ -4,6 +4,8 @@ module.exports = function ({ dir, callback }) {
   const files = readdir(dir);
   for (let i = 0; i < files.length; i++) {
     const out = callback(fs.readFileSync(files[i], 'utf8'));
+    // if not string, do nothing.
+    if (typeof out !== 'string') return;
     fs.writeFileSync(files[i], out, 'utf8');
   }
 };
